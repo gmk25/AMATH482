4d60fb44d0f0f600e229d3ab9b3a696f4aca27b9
gmk25 <gmk6@uw.edu>
2020-03-14 10:04:09 -0700

    initial push

---
+++ AMATH482_HW4.m
@@ -0,0 +1,284 @@
+%% Test 1
+%Checkie Brown
+[y,Fs] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Checkie_Brown/hey/Checkie_Brown_-_12_-_Rosalie_CB_29.mp3');
+[y12,Fs12] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Checkie_Brown/hey/Checkie_Brown_-_01_-_Susie_the_Cat_-_Boss_CB_25.mp3');
+[y16,Fs16] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Checkie_Brown/hey/Checkie_Brown_-_06_-_Gangster_Downhill_-_Hippie_Beatnix_CB_26.mp3');
+[y24,Fs24] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Checkie_Brown/Staubkeller_Live/Checkie_Brown_-_05_-_Wirklich_Wichtig_CB_011.mp3');
+%Dee Yan-Key
+[y1, Fs1] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Dee_Yan-Key/Thalatta_Thalatta/Dee_Yan-Key_-_03_-_Triton.mp3');
+[y13,Fs13] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Dee_Yan-Key/Thalatta_Thalatta/Dee_Yan-Key_-_02_-_Amphitrite.mp3');
+[y17,Fs17] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Dee_Yan-Key/Perpetual_Peace/Dee_Yan-Key_-_03_-_World_of_Brothers___Allegretto.mp3');
+[y25,Fs25] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Dee_Yan-Key/Perpetual_Peace/Dee_Yan-Key_-_01_-_Wars_End___Allegro_molto_moderato.mp3');
+%Derek Clegg
+[y2,Fs2] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Derek_Clegg/Solar/Derek_Clegg_-_12_-_Annalise.mp3');
+[y14,Fs14] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Derek_Clegg/Solar/Derek_Clegg_-_08_-_Cest_La_Vie.mp3');
+[y15,Fs15]=webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Derek_Clegg/Solar/Derek_Clegg_-_07_-_Ill_Almost_Get_Us_There.mp3');
+[y26,Fs26] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Derek_Clegg/Solar/Derek_Clegg_-_03_-_Misunderstood.mp3');
+%% Test 2- Classical
+[y8, Fs8] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Julie_Maxwells_Piano_Music/Farther_Than_All_The_Stars/Julie_Maxwells_Piano_Music_-_09_-__Sakura.mp3');
+[y18,Fs18] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Julie_Maxwells_Piano_Music/Classic_Piano_Collection_from_the_Princess_of_Mars/Julie_Maxwells_Piano_Music_-_10_-__Starry_Sky.mp3');
+[y19,Fs19] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Julie_Maxwells_Piano_Music/Classic_Piano_Collection_from_the_Princess_of_Mars/Julie_Maxwells_Piano_Music_-_11_-_Labyrinth_Dreams.mp3');
+
+[y9, Fs9] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Oddio_Overplay/Felipe_Sarro/Maurice_Ravel/Felipe_Sarro_-_15_-_Ravel_-_Bolro_transcribed_by_Branga.mp3');
+[y20,Fs20] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Oddio_Overplay/Felipe_Sarro/Bach_Original_works_and_transcriptions/Felipe_Sarro_-_18_-_Bach_Prelude_BWV_855a_Siloti_transcription.mp3');
+[y21,Fs21] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Oddio_Overplay/Felipe_Sarro/Bach_Original_works_and_transcriptions/Felipe_Sarro_-_12_-_Bach_Partita_2_BWV_1004_Chaconne_Siloti_transcription.mp3');
+
+[y10, Fs10] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Vol_4_Satie_Rearranged_Furniture_Music/Chad_Crouch_-_06_-_Gnossienne_3.mp3');
+[y22,Fs22] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Field_Report_Vol_I_Oaks_Bottom_Instrumental/Chad_Crouch_-_04_-_The_Spring_Instrumental.mp3');
+[y23,Fs23] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Field_Report_Vol_I_Oaks_Bottom_Instrumental/Chad_Crouch_-_The_Chorus_Ceases_Instrumental.mp3');
+%%
+[y3, Fs3] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/none_given/Scott_Holmes/Scott_Holmes_-_Singles/Scott_Holmes_-_Driven_To_Success.mp3');
+[y5,Fs5] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Unheard_Music_Concepts/The_Lasso_of_Time/Unheard_Music_Concepts_-_16_-_Dakota.mp3');
+[y6,Fs6] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/J_Syreus_Bach/It_Rains__Abstract_Jazz/J_Syreus_Bach_-_Goodbye_Grandmother.mp3');
+[y7,Fs7] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Field_Report_Vol_I_Oaks_Bottom_Instrumental/Chad_Crouch_-_The_Chorus_Ceases_Instrumental.mp3');
+[y11,Fs11] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Decoder_Magazine/Lately_Kind_of_Yeah/Poindexter/Lately_Kind_of_Yeah_-_14_-_Goner.mp3');
+%% Test 1- Different artists over different genres
+%Brown
+[St_specb,St_specbmed, St_specbend] = discretizespec(y,Fs);
+[St_specb1,St_specbmed1, St_specbend1] = discretizespec(y12,Fs12);
+[St_specb2,St_specbmed2, St_specbend2] = discretizespec(y16,Fs16);
+[St_specb3,St_specbmed3, St_specbend3] = discretizespec(y24,Fs24);
+%Yan-Key
+[St_specy,St_specymed, St_specyend] = discretizespec(y1,Fs1);
+[St_specy1,St_specymed1, St_specyend1] = discretizespec(y13,Fs13);
+[St_specy2,St_specymed2, St_specyend2] = discretizespec(y17,Fs17);
+[St_specy3,St_specymed3, St_specyend3] = discretizespec(y25,Fs25);
+%Clegg
+[St_specc,St_speccmed, Stspeccend] = discretizespec(y2,Fs2);
+[St_specc1,St_speccmed1, Stspeccend1] = discretizespec(y14,Fs14);
+[St_specc2,St_speccmed2, Stspeccend2] = discretizespec(y15,Fs15);
+[St_specc3,St_speccmed3, Stspeccend3] = discretizespec(y26,Fs26);
+%%
+feature = 7;
+[U_t1,w,w2,sortgroupb, sortgroupy, sortgroupc, sortgroupb_proj2, sortgroupy_proj2, sortgroupc_proj2,ms1,ms2,ms3,ms1_proj2,ms2_proj2,ms3_proj2] = PCAandLDA(feature,[St_specb,St_specbmed, St_specbend,St_specb1,St_specbmed1, St_specbend1,St_specb2,St_specbmed2, St_specbend2,St_specb3,St_specbmed3, St_specbend3], [St_specy,St_specymed, St_specyend,St_specy1,St_specymed1, St_specyend1,St_specy2,St_specymed2, St_specyend2,St_specy3,St_specymed3, St_specyend3], [St_specc,St_speccmed, Stspeccend,St_specc1,St_speccmed1, Stspeccend1,St_specc2,St_speccmed2, Stspeccend2,St_specc3,St_speccmed3, Stspeccend3]);
+%%
+plot(sortgroupb,sortgroupb_proj2,'ro',sortgroupy,sortgroupy_proj2,'ko',sortgroupc,sortgroupc_proj2,'bo')
+hold on
+plot(ms1,ms1_proj2,'r*',ms2,ms2_proj2,'k*',ms3,ms3_proj2,'b*');
+title('Test 1 training data')
+legend('Checkie Brown','Dee Yan-Key','Derek Clegg')
+%% Test data
+[yt1,Fst1] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Checkie_Brown/hey/Checkie_Brown_-_08_-_Hippie_Bulle_-Stoned_Funghi_CB_28.mp3');
+[yt2,Fst2] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Dee_Yan-Key/Thalatta_Thalatta/Dee_Yan-Key_-_01_-_Poseidon.mp3');
+[yt3,Fst3] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Derek_Clegg/Solar/Derek_Clegg_-_08_-_Cest_La_Vie.mp3');
+[Spectest1beg, Spectest1med, Spectest1end] = discretizespec(yt1,Fst1);
+[Spectest2beg, Spectest2med, Spectest2end] = discretizespec(yt2,Fst2);
+[Spectest3beg, Spectest3med, Spectest3end] = discretizespec(yt3,Fst3);
+spectotal = [Spectest1beg, Spectest1med, Spectest1end,Spectest2beg, Spectest2med, Spectest2end,Spectest3beg, Spectest3med, Spectest3end];
+%minimize the distance between the pvals and means
+hiddenlabels = [1,1,1,2,2,2,3,3,3];
+Resvec = zeros(1,size(spectotal,2));
+    TestNum = size(spectotal,2);
+for i = 1:TestNum
+    TestMat = U_t1'*spectotal(:,i);
+    pval = w'*TestMat;
+    pval2 = w2'*TestMat;
+    dif1 = ([ms1,ms1_proj2]-[pval,pval2]);
+    dif2 = ([ms2,ms2_proj2]-[pval,pval2]);
+    dif3 = ([ms3,ms3_proj2]-[pval,pval2]);
+    diftotal = [abs(dif1), abs(dif2), abs(dif3)];
+    [~,col] = min(diftotal);
+    if(col==1)
+        Resvec(1,i) = 1;
+    elseif(col==2)
+        Resvec(1,i) = 2;
+    else
+        Resvec(1,i) = 3;
+    end
+end
+    error = Resvec-hiddenlabels;
+    count = find(error==0);
+    succRate = length(count)/length(Resvec);
+%% Test 2- Different artists over same genre
+%JM
+[St_specjm,St_specjmmed,St_specjmend] = discretizespec(y8,Fs8);
+[St_specjm1,St_specjmmed1,St_specjmend1] = discretizespec(y18,Fs18);
+[St_specjm2,St_specjmmed2,St_specjmend2] = discretizespec(y19,Fs19);
+%SS
+[St_specss, St_specssmed,St_specssend] = discretizespec(y9,Fs9);
+[St_specss1, St_specssmed1,St_specssend1] = discretizespec(y20,Fs20);
+[St_specss2, St_specssmed2,St_specssend2] = discretizespec(y21,Fs21);
+%CC
+[St_speccc, St_specccmed,Stspecccend] = discretizespec(y10,Fs10);
+[St_speccc1, St_specccmed1,Stspecccend1] = discretizespec(y22,Fs22);
+[St_speccc2, St_specccmed2,Stspecccend2] = discretizespec(y23,Fs23);
+%%
+feature = 3;
+[U_t2,w,w2,sortgroupjm, sortgroupss, sortgroupcc,sortgroupjm_proj2, sortgroupss_proj2, sortgroupcc_proj2,ms1,ms2,ms3,ms1_proj2,ms2_proj2,ms3_proj2] = PCAandLDA(feature,[St_specjm,St_specjmmed,St_specjmend,St_specjm1,St_specjmmed1,St_specjmend1,St_specjm2,St_specjmmed2,St_specjmend2], [St_specss, St_specssmed,St_specssend,St_specss1, St_specssmed1,St_specssend1,St_specss2, St_specssmed2,St_specssend2], [St_speccc, St_specccmed,Stspecccend,St_speccc1, St_specccmed1,Stspecccend1,St_speccc2, St_specccmed2,Stspecccend2]);
+%%
+plot(sortgroupjm,sortgroupjm_proj2,'ro',sortgroupss,sortgroupss_proj2,'ko',sortgroupcc,sortgroupcc_proj2,'bo')
+hold on
+plot(ms1,ms1_proj2,'r*',ms2,ms2_proj2,'k*',ms3,ms3_proj2,'b*');
+title('Test 2 training data')
+legend('Julie Maxwell','Felipe Sarro','Chad Crouch')
+%%
+[yt1,Fst1] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Julie_Maxwells_Piano_Music/Classic_Piano_Collection_from_the_Princess_of_Mars/Julie_Maxwells_Piano_Music_-_07_-_Sleeping_Rose.mp3');
+[yt2,Fst2] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Oddio_Overplay/Felipe_Sarro/Bach_Original_works_and_transcriptions/Felipe_Sarro_-_09_-_Bach_Prelude_8_BWV_853.mp3');
+[yt3,Fst3] = webread('https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Field_Report_Vol_I_Oaks_Bottom_Instrumental/Chad_Crouch_-_Stratum_Instrumental.mp3');
+[Spectest1beg, Spectest1med, Spectest1end] = discretizespec(yt1,Fst1);
+[Spectest2beg, Spectest2med, Spectest2end] = discretizespec(yt2,Fst2);
+[Spectest3beg, Spectest3med, Spectest3end] = discretizespec(yt3,Fst3);
+spectotal = [Spectest1beg, Spectest1med, Spectest1end,Spectest2beg, Spectest2med, Spectest2end,Spectest3beg, Spectest3med, Spectest3end];
+%minimize the distance between the pvals and means
+hiddenlabels = [1,1,1,2,2,2,3,3,3];
+Resvec = zeros(1,size(spectotal,2));
+    TestNum = size(spectotal,2);
+for i = 1:TestNum
+    TestMat = U_t2'*spectotal(:,i);
+    pval = w'*TestMat;
+    pval2 = w2'*TestMat;
+    dif1 = ([ms1,ms1_proj2]-[pval,pval2]);
+    dif2 = ([ms2,ms2_proj2]-[pval,pval2]);
+    dif3 = ([ms3,ms3_proj2]-[pval,pval2]);
+    diftotal = [abs(dif1), abs(dif2), abs(dif3)];
+    [~,col] = min(diftotal);
+    if(col==1)
+        Resvec(1,i) = 1;
+    elseif(col==2)
+        Resvec(1,i) = 2;
+    else
+        Resvec(1,i) = 3;
+    end
+end
+    error = Resvec-hiddenlabels;
+    count = find(error==0);
+    succRate = length(count)/length(Resvec);
+%% Test 3 - Different genres
+%using classical, rock, and jazz from above. Only loading some new ones in.
+[St_specsh,St_specshmed,St_specshend] = discretizespec(y3,Fs3); %rock
+[St_specy,St_specymed,St_specyend] = discretizespec(y11,Fs11); %rock
+[St_specu,St_specumed,St_specuend] = discretizespec(y5,Fs5); %jazz
+[St_specjb,St_specjbmed,St_specjbend] = discretizespec(y6,Fs6); %jazz
+feature = 10;
+[U_t3,w,w2,sortgroupjazz, sortgroupclass, sortgrouprock,sortgroupjazz_proj2, sortgroupclass_proj2, sortgrouprock_proj2,ms1,ms2,ms3,ms1_proj2,ms2_proj2,ms3_proj2 ] = PCAandLDA(feature, [St_specb,St_specbmed, St_specbend,St_specb1,St_specbmed1, St_specbend1,St_specb2,St_specbmed2, St_specbend2,St_specu,St_specumed,St_specuend,St_specjb,St_specjbmed,St_specjbend], [St_specy,St_specymed, St_specyend,St_specy1,St_specymed1, St_specyend1,St_specy2,St_specymed2, St_specyend2,St_specjm,St_specjmmed,St_specjmend,St_specss1, St_specssmed1,St_specssend1], [St_specc,St_speccmed, Stspeccend,St_specc1,St_speccmed1, Stspeccend1,St_specc2,St_speccmed2, Stspeccend2,St_specsh,St_specshmed,St_specshend,St_specy,St_specymed,St_specyend]);
+plot(sortgroupjazz,sortgroupjazz_proj2,'ro',sortgroupclass,sortgroupclass_proj2,'ko',sortgrouprock,sortgrouprock_proj2,'bo')
+hold on
+plot(ms1,ms1_proj2,'r*',ms2,ms2_proj2,'k*',ms3,ms3_proj2,'b*');
+title('Test 3 training data')
+legend('Jazz','Classical','Rock')
+%%
+%using most data from above as "testing data"
+[Spectest1beg, Spectest1med, Spectest1end] = discretizespec(y12,Fs12); %jazz
+[Spectest2beg, Spectest2med, Spectest2end] = discretizespec(y11,Fs11); %rock
+[Spectest3beg, Spectest3med, Spectest3end] = discretizespec(y8,Fs8); %classical
+spectotal = [Spectest1beg, Spectest1med, Spectest1end,Spectest2beg, Spectest2med, Spectest2end,Spectest3beg, Spectest3med, Spectest3end];
+hiddenlabels = [1,1,1,2,2,2,3,3,3];
+Resvec = zeros(1,size(spectotal,2));
+    TestNum = size(spectotal,2);
+for i = 1:TestNum
+    TestMat = U_t3'*spectotal(:,i);
+    pval = w'*TestMat;
+    pval2 = w2'*TestMat;
+    dif1 = ([ms1,ms1_proj2]-[pval,pval2]);
+    dif2 = ([ms2,ms2_proj2]-[pval,pval2]);
+    dif3 = ([ms3,ms3_proj2]-[pval,pval2]);
+    diftotal = [abs(dif1), abs(dif2), abs(dif3)];
+    [~,col] = min(diftotal);
+    if(col==1)
+        Resvec(1,i) = 1;
+    elseif(col==2)
+        Resvec(1,i) = 2;
+    else
+        Resvec(1,i) = 3;
+    end
+end
+    error = Resvec-hiddenlabels;
+    count = find(error==0);
+    succRate = length(count)/length(Resvec);
+%%
+function[U,w,w2,sortgroup1, sortgroup2, sortgroup3, sortgroup1_proj2, sortgroup2_proj2, sortgroup3_proj2,ms1,ms2,ms3,ms1_proj2,ms2_proj2,ms3_proj2] = PCAandLDA(feature, St_spec1, St_spec2, St_spec3)
+n1 = size(St_spec1,2); n2 = size(St_spec2,2); n3 = size(St_spec3,2);
+[U S V] = svd([St_spec1, St_spec2,St_spec3],'econ');
+proj = S*V.';
+U = U(:,1:feature);
+group1 = proj(1:feature,1:n1);
+group2 = proj(1:feature,n1+1:n1+n2);
+group3 = proj(1:feature,n1+n2+1:n1+n2+n3);
+
+m = mean(proj(1:feature,:),2);
+m1 = mean(group1,2);
+m2 = mean(group2,2);
+m3 = mean(group3, 2);
+
+Sw = 0; %within class variances
+for k = 1:n1
+    Sw = Sw+ (group1(:,k)-m1)*(group1(:,k)-m1)';
+end
+for k = 1:n2
+    Sw = Sw+ (group2(:,k)-m2)*(group2(:,k)-m2)';
+end
+for k = 1:n3
+    Sw = Sw+ (group3(:,k)-m3)*(group3(:,k)-m3)';
+end
+
+Sb = 0;
+Sb = Sb+(m1-m)*(m1-m)';
+Sb = Sb+(m2-m)*(m2-m)';
+Sb = Sb+(m3-m)*(m3-m)';
+[V2, D] = eig(Sb,Sw); %LDA
+[~,ind] = max(abs(diag(D))); %set max equal to zero and find eigenvector
+w = V2(:,ind);
+w = w/norm(w,2);
+
+vgroup1 = w'*group1;
+vgroup2 = w'*group2;
+vgroup3 = w'*group3;
+
+sortgroup1 = sort(vgroup1);
+sortgroup2 = sort(vgroup2);
+sortgroup3 = sort(vgroup3);
+
+ms1 = mean(sortgroup1);
+ms2 = mean(sortgroup2);
+ms3 = mean(sortgroup3);
+
+%proj 2
+D(ind,ind) = 0;
+[~,ind] = max(abs(diag(D))); %set max equal to zero and find eigenvector
+w2 = V2(:,ind);
+w2 = w2/norm(w2,2);
+
+vgroup1_proj2 = w2'*group1;
+vgroup2_proj2 = w2'*group2;
+vgroup3_proj2 = w2'*group3;
+sortgroup1_proj2 = sort(vgroup1_proj2);
+sortgroup2_proj2 = sort(vgroup2_proj2);
+sortgroup3_proj2 = sort(vgroup3_proj2);
+
+ms1_proj2 = mean(sortgroup1_proj2);
+ms2_proj2 = mean(sortgroup2_proj2);
+ms3_proj2 = mean(sortgroup3_proj2);
+end
+
+function [St_spec,St_specmed,St_spec1] = discretizespec(y,Fs)
+Fs = 44100/10;
+v = y.';
+sig = (v(1,:)+v(2,:))./2;
+sig = sig(1:10:length(sig));
+L = 5;
+n = Fs*L; %chose my own n
+v = sig(1:n); %make into five-second clip
+vmed = sig(end-n+1-n:end-n);
+v1 = sig(end-n+1:end);
+t2 = linspace(0,L,n+1);
+t = t2(1:n);
+k = ((1)/L)*[0:n/2-1 -n/2:-1];
+% Matlab spec function
+tslide = 0:0.1:L;
+a = 1;
+St_spec = zeros(length(tslide),n);
+St_specmed = zeros(length(tslide),n);
+St_spec1 = zeros(length(tslide),n);
+for b = 1:length(tslide)
+g1 = exp(-a*(t-tslide(b)).^2);
+St = fft(g1.*v); %St1 is in the frequency domain
+Stmed = fft(g1.*vmed);
+St1 = fft(g1.*v1);
+St_spec(b, :) = fftshift(abs(St));
+St_specmed(b,:) = fftshift(abs(Stmed));
+St_spec1(b,:) = fftshift(abs(St1));
+end
+St_spec = reshape(St_spec,[length(tslide)*length(St_spec),1]);
+St_specmed = reshape(St_specmed,[length(tslide)*length(St_specmed),1]);
+St_spec1 = reshape(St_spec1,[length(tslide)*length(St_spec1),1]);
+end
